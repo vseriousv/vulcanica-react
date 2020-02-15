326340d7cbbe233248768ed7ed14247972dfd329
Gavrilov Kirill <gavrilovskirill@gmail.com>
2020-02-15 13:27:01 +0700

    Create react app and support sccs

---
+++ src/components/screen1/ViewScreen1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import ControllerScreen1 from './ControllerScreen1';
+import './StyleScreen1.scss';
+
+
+export default class ViewScreen1 extends React.Component {
+    render(){
+        return(
+            <div className="test">
+
+            </div>
+        );
+    }
+}
