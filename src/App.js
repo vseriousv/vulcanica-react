0763d3b963797618288b34e336712276c8f26160
Gavrilov Kirill <gavrilovskirill@gmail.com>
2020-02-15 14:52:49 +0700

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.scss';
 // import ViewScreen1 from './components/screen1/ViewScreen1';
 // import ViewScreen2 from './components/screen2/ViewScreen2';
-import ViewScreen3 from './components/screen3/ViewScreen3';
+// import ViewScreen3 from './components/screen3/ViewScreen3';
 // import ViewScreen4 from './components/screen4/ViewScreen4';
 // import ViewScreen5 from './components/screen5/ViewScreen5';
-import ViewScreen6 from './components/screen6/ViewScreen6';
+// import ViewScreen6 from './components/screen6/ViewScreen6';
 // import ViewScreen7 from './components/screen7/ViewScreen7';
 // import ViewScreen8 from './components/screen8/ViewScreen8';
 
@@ -15,10 +15,10 @@ function App() {
     <div id="backgroudnFullPage" className="backgroudnFullPage">
       {/* <ViewScreen1 /> */}
       {/* <ViewScreen2 /> */}
-      <ViewScreen3 />
+      {/* <ViewScreen3 /> */}
       {/* <ViewScreen4 /> */}
       {/* <ViewScreen5 /> */}
-      <ViewScreen6 />
+      {/* <ViewScreen6 /> */}
       {/* <ViewScreen7 /> */}
       {/* <ViewScreen8 /> */}
     </div>
