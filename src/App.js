82aabb30a9956c45babfabaead72be497b5d1a76
Vladimir Mihaylov <zombotv82@gmail.com>
2020-02-15 14:38:26 +0700

    Viewscreen3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import ViewScreen1 from './components/screen1/ViewScreen1';
-import ViewScreen2 from './components/screen2/ViewScreen2';
+// import ViewScreen1 from './components/screen1/ViewScreen1';
+// import ViewScreen2 from './components/screen2/ViewScreen2';
+import ViewScreen3 from './components/screen3/ViewScreen3';
+// import ViewScreen4 from './components/screen4/ViewScreen4';
+// import ViewScreen5 from './components/screen5/ViewScreen5';
+// import ViewScreen6 from './components/screen6/ViewScreen6';
+// import ViewScreen7 from './components/screen7/ViewScreen7';
+// import ViewScreen8 from './components/screen8/ViewScreen8';
+
 
 function App() {
   return (
     <div className="App">
       {/* <ViewScreen1 /> */}
-      <ViewScreen2 />
+      {/* <ViewScreen2 /> */}
+      <ViewScreen3 />
+      {/* <ViewScreen4 /> */}
+      {/* <ViewScreen5 /> */}
+      {/* <ViewScreen6 /> */}
+      {/* <ViewScreen7 /> */}
+      {/* <ViewScreen8 /> */}
     </div>
   );
 }
